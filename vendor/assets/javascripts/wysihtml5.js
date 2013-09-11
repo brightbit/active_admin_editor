2046b2004948e4f34661e01525a74a1051926775
Nick Hoffman <nick@deadorange.com>
2013-09-10 21:35:35 -0400

    "url", "src", and "href" parser rules allow values to begin with "//".
    This is necessary because many URLs omit the protocol. E.g.
    <a href="//foo.com">
    
    Note: The updated tests pass.

--- vendor/assets/javascripts/wysihtml5.js
+++ vendor/assets/javascripts/wysihtml5.js
@@ -5062,7 +5062,7 @@ wysihtml5.dom.parse = (function() {
   // ------------ attribute checks ------------ \\
   var attributeCheckMethods = {
     url: (function() {
-      var REG_EXP = /^https?:\/\//i;
+      var REG_EXP = /^(https?:)?\/\//i;
       return function(attributeValue) {
         if (!attributeValue || !attributeValue.match(REG_EXP)) {
           return null;
@@ -5074,7 +5074,7 @@ wysihtml5.dom.parse = (function() {
     })(),
 
     src: (function() {
-      var REG_EXP = /^(\/|https?:\/\/)/i;
+      var REG_EXP = /^(\/\/?|https?:\/\/)/i;
       return function(attributeValue) {
         if (!attributeValue || !attributeValue.match(REG_EXP)) {
           return null;
@@ -5086,7 +5086,7 @@ wysihtml5.dom.parse = (function() {
     })(),
 
     href: (function() {
-      var REG_EXP = /^(\/|https?:\/\/|mailto:)/i;
+      var REG_EXP = /^(\/\/?|https?:\/\/|mailto:)/i;
       return function(attributeValue) {
         if (!attributeValue || !attributeValue.match(REG_EXP)) {
           return null;
