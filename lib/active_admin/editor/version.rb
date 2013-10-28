24cec1f32fd9c094c409e18974dd626cff4c70a3
Jonathan Camenisch <jonathan@camenisch.net>
2013-10-28 10:29:58 -0500

    Upgrade all dependencies to incorporate security fixes

--- lib/active_admin/editor/version.rb
+++ lib/active_admin/editor/version.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   module Editor
     # Abuse semver here to indicate a change:
     # This is version 1.1.0, upgraded with
-    # wysihtml5 0.4.0pre
-    VERSION = '1.1.0.0.4.0pre'
+    # wysihtml5 0.4.0pre, and with all depencies updated
+    VERSION = '1.1.0-bb.0.4.0pre'
   end
 end
